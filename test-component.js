80e63c9fd966d3895161a430a81f9f10c7c5c700
台灣智能經貿協會 <tribeorgtw@gmail.com>
2025-08-09 18:29:38 +0800

    [--檢查點--] 清除一些廢碼

--- test-component.js
+++
@@ -1,2 +0,0 @@
-console.log('ThemeSwitcher available:', \!\!window.ThemeSwitcher);
-if (window.BrandThemeManager) { console.log('Available themes:', window.BrandThemeManager.getAvailableThemes()); console.log('Current theme:', window.BrandThemeManager.getCurrentTheme()); }
